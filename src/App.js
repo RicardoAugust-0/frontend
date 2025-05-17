94e2151a2d8b89a494ebe4b0b6a6100092eec4ad
RicardoAugust-0 <tatianejc10@gmail.com>
2025-05-16 21:45:37 -0300

    inicio de projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import { useEffect, useRef, useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const videos = [
+        '/media/background1.mp4',
+        '/media/background2.mp4',
+        '/media/background3.mp4'
+    ];
+
+    const [currentVideo, setCurrentVideo] = useState(0);
+    const [fade, setFade] = useState(false);
+    const videoRef = useRef(null);
+
+    const handleVideoEnd = () => {
+        // Inicia o efeito de fade out 
+        setFade(true);
+
+        setTimeout(() => {
+            setCurrentVideo((prev) => (prev + 1) % videos.length);
+            setFade(false);
+        }, 500);
+    };
+
+    useEffect(() => {
+        const video = videoRef.current;
+        if (video) {
+            video.load();
+            video.play();
+        }
+    }, [currentVideo]);
+
+    return (
+        <div className="relative w-full h-screen overflow-hidden">
+            {/* Fundo preto fixo */}
+            <div className="fixed top-0 left-0 w-full h-full bg-black z-0" />
+
+            {/* Vídeo por cima com fade */}
+            <video
+                ref={videoRef}
+                onEnded={handleVideoEnd}
+                muted
+                playsInline
+                autoPlay
+                className={`object-cover w-full h-full fixed top-0 left-0 z-10 transition-opacity duration-500 ${fade ? 'opacity-0' : 'opacity-100'
+                    }`}
+            >
+                <source src={videos[currentVideo]} type="video/mp4" />
+                Seu navegador não suporta vídeos.
+            </video>
+        </div>
+    );
 }
 
 export default App;
