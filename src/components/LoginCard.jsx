394898b36e3c96fa9c491c8f29af97625c40a59a
Ricardo Augusto <tatianejc11@gmail.com>
2025-05-18 01:16:26 -0300

    `Refactor LoginCard component: updated styles, icons, and input fields`

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -2,48 +2,51 @@ import { Mail, Lock } from "lucide-react";
 
 function LoginCard() {
     return (
-        <div className="bg-white bg-opacity-90 rounded-xl shadow-xl p-8 w-96 text-center backdrop-blur-md">
+        // Card inteiro
+        <div className="bg-white shadow-[inset_0px_0px_4px_1px_#b3ffcf] bg-opacity-90 rounded-xl  p-8 w-96 text-center backdrop-blur-md">
             <img
                 src="imgs/logo-moovox-redimensionado.png"
                 alt="Moovox Logo"
-                className="mx-auto mb-4 w-20"
+                className="mx-auto mb-4 w-28"
+                draggable="false"
             />
 
-            <h2 className="font-poppins text-2xl font-semibold text-green-700 mb-1">
-                Bem-vindo ao <span className="text-brown-700 font-bold">Moovox</span>
+            <h2 className="font-poppins text-2xl text-green-700 mb-1">
+                Bem-vindo ao <span className="text-3xl text-amber-950 font-bold">Moovox</span>
             </h2>
             <p className="text-sm text-gray-600 mb-6 font-poppins">
                 Gestão inteligente de animais no campo
             </p>
 
             <form className="space-y-4 text-left">
-                <div>
-                    <label className="block text-sm text-gray-700 mb-1">E-mail</label>
-                    <div className="flex items-center border border-gray-300 rounded-md px-3">
+                <div> {/* Input Email */}
+                    <label className="block font-poppins text-sm text-gray-700 mb-1">E-mail</label>
+                    <div className="flex bg-green-100 items-center border border-gray-300 rounded-xl pl-3">
                         <Mail className="w-4 h-4 text-gray-700 mr-2" />
                         <input
                             type="email"
                             placeholder="Digite seu e-mail"
-                            className="w-full py-2 text-sm outline-none bg-transparent"
+                            className="w-full font-poppins py-2 text-sm outline-none bg-transparent transition-all duration-150 ease-in-out focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 focus:border-blue-400 rounded-e-xl"
                         />
                     </div>
                 </div>
 
-                <div>
-                    <label className="block text-sm text-gray-700 mb-1">Senha</label>
-                    <div className="flex items-center border border-gray-300 rounded-md px-3">
+                <div> {/* Input Senha */}
+                    <label className="block font-poppins text-sm text-gray-700 mb-1">Senha</label>
+                    <div className="flex bg-green-100 items-center border border-gray-300 rounded-xl pl-3">
                         <Lock className="w-4 h-4 text-gray-700 mr-2" />
                         <input
                             type="password"
                             placeholder="Digite sua senha"
-                            className="w-full py-2 text-sm outline-none bg-transparent focus:outline-blue-600"
+                            className="w-full font-poppins py-2 text-sm outline-none bg-transparent transition-all duration-150 ease-in-out focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 focus:border-blue-400 rounded-e-xl"
                         />
                     </div>
                 </div>
 
+                {/* Botão Entrar + Esqueci minha senha */}
                 <button
                     type="submit"
-                    className="w-full bg-green-600 text-white py-2 rounded-xl hover:bg-green-700 transition"
+                    className="w-full font-poppins font-bold bg-green-600 text-white p-[5px] rounded-xl hover:bg-green-700 transition"
                 >
                     Entrar
                 </button>
@@ -51,8 +54,7 @@ function LoginCard() {
 
             <a
                 href="/"
-                className="block text-sm text-green-600 mt-4 hover:underline font-poppins"
-            >
+                className="block text-sm text-green-600 mt-4 hover:underline font-poppins">
                 Esqueci minha senha
             </a>
         </div>
